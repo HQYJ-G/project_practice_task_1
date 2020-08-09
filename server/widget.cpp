0c9d9d907452be6b4286e3719cf0728c446b11e5
xiong <727257758@qq.com>
2020-08-09 23:03:22 +0800

    数据库实现

--- server/widget.cpp
+++ server/widget.cpp
@@ -6,13 +6,22 @@ Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
+    sBufType temp;
+
     ui->setupUi(this);
     Sqlite sql("employeeinfo.db");
     sql.CreateTable("info"," ID INTEGER PRIMARY KEY   AUTOINCREMENT,NAME TEXT NOT NULL,AGE INT ,GENDER TEXT,TEL TEXT, WAGE INT, AUTH TEXT, PWD TEXT");
     sql.Insert("info","NAME,AGE,GENDER","\"X\",18,\"man\"");
     sql.Insert("info","NAME,AGE,GENDER","\"s\",18,\"man\"");
+    sql.Select("info","ID = 1");
+    while(!sql.Buf.empty())
+    {
+        temp = sql.Buf.front();
+        sql.Buf.pop();
+        cout<<temp.key<<"="<<temp.val<<endl;
+    }
  //   sql.Updata("info","NAME = \"X\"","AGE = 20");
-    sql.Delect("info","AGE = 20");
+//    sql.Delect("info","AGE = 20");
 }
 
 Widget::~Widget()
